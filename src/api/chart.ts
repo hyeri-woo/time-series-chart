8cad475d012fe5a277fd9227fe9002e5b7355691
hyeri-woo <woohyeri0525@gmail.com>
2023-09-13 17:10:50 +0900

    Chore: 배포 url 추가

--- src/api/chart.ts
+++ src/api/chart.ts
@@ -1,7 +1,9 @@
 import Http from './http';
 import { DataItemType } from '../types';
 
-const searchHttp = new Http(`${process.env.REACT_APP_DEV_URL}`);
+const BASE_URL =
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_DEPLOY_URL : process.env.REACT_APP_DEV_URL;
+const searchHttp = new Http(BASE_URL || '');
 
 export const getChartData = async (): Promise<{ [date: string]: DataItemType }> => {
   return await searchHttp.get<any>('/response');
